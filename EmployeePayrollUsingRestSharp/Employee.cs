3cf5a14bfefa99c4e0fdb26a63222b178bcd8bd6
Satantra Tewari <satantra1508@gmail.com>
2020-11-20 11:48:18 +0530

    [satantra] initial commit. files added

---
+++ EmployeePayrollUsingRestSharp/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollUsingRestSharp
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int id { get; set; }
+        public int salary { get; set; }
+    }
+}
