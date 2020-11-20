c09a21f2d458f6b77697047b0c79709637cf1791
Satantra Tewari <satantra1508@gmail.com>
2020-11-20 18:53:24 +0530

    [satantra] employee added

--- EmployeePayrollUsingRestSharp/UnitTest1.cs
+++ EmployeePayrollUsingRestSharp/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -32,6 +33,21 @@ namespace EmployeePayrollUsingRestSharp
             Assert.AreEqual(7, employeeList.Count);
             foreach(var i in employeeList)
                 Console.WriteLine("ID: "+i.id+"\nNAME: "+i.Name+"\nSALARY: "+i.salary);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //returns added employee
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            JObject object= new JObject();
+            object.Add("name", "Axl Rose");      
+            object.Add("salary", 90000);
+            request.AddParameter("application/json", object, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Employee employeeList = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
+            Assert.AreEqual("Axl Rose", employeeList.Name);
+            Assert.AreEqual(90000, employeeList.salary);
         }
     }
 }
